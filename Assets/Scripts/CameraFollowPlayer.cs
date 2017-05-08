8757dd6677b656e3f8ef2711b98ec430add18081
X1_Carbon\Milan <milan.pingel@gmail.com>
2017-05-08 20:42:03 +0200

    1st lesson

---
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour {
+
+	Transform _player;
+	Transform player {
+		get {
+			if (_player == null)
+				_player = FindObjectOfType<PlayerMovement> ().transform;
+			return _player;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (player.position.x, transform.position.y, player.position.z);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class PlayerMovement : MonoBehaviour {
+
+	[System.Serializable]
+	public class Controls {
+		[SerializeField]
+		public string horizontalAxis;
+		[SerializeField]
+		public string verticalAxis;
+		[SerializeField]
+		public KeyCode aimButton;
+	}
+
+	Rigidbody _playerRigidbody;
+	Rigidbody playerRigidbody {
+		get {
+			if (_playerRigidbody == null)
+				_playerRigidbody = GetComponent<Rigidbody> ();
+			return _playerRigidbody;
+		}
+	}
+
+	public Controls playerControls;
+
+	public float _speed = 5f;
+	public float speed {
+		get {
+			if (Input.GetKey(playerControls.aimButton))
+				return _speed / 2;
+			else
+				return _speed;
+		}
+		set {
+			_speed = value;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		Move (direction);
+		Aim ();
+	}
+
+	void Move(Vector2 movement) {
+		playerRigidbody.velocity = new Vector3 (speed*movement.x, playerRigidbody.velocity.y, speed*movement.y);
+		Debug.Log (movement);
+	}
+
+	void Aim() {
+		Vector3 mouseScreenPosition = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.y);
+		Debug.Log (mouseScreenPosition);
+		Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mouseScreenPosition);
+		transform.LookAt (new Vector3(mouseWorldPosition.x, transform.position.y, mouseWorldPosition.z));
+	}
+
+}
